ff3da8808c1bcfc541864fdb64b7d0214f5e4441
Hacene Tchoketch-kebir <hassentchoketch@gmail.com>
2023-04-11 14:22:18 +0100

    Update File

--- preprocessing_data.py
+++ preprocessing_data.py
@@ -1,27 +1,31 @@
 import os
 from helper import  *
 
+import warnings
+warnings.filterwarnings('ignore')
+
+
 def main():
     cwd = os.getcwd()
 
-    inf = load_transform_data(path='DZ_Consumption_price_index.csv')
+    inf = load_transform_data(data_file='DZ_Consumption_price_index.csv')
     # print(inf.shape) (300, 1) 
     # print(inf.columns) CPI(%)
     if not os.path.exists(os.path.join(cwd, "results")):
       os.mkdir(cwd + "\\results") 
     if not os.path.exists(os.path.join(cwd+'\\results', "graphs")):
         os.mkdir(cwd + "\\results\\graphs")
-    graghs_path = cwd + "\\results\\graphs"
+    # graghs_path = cwd + "\\results\\graphs"
 
     plot_description(time_series=inf, title='Algerian Inflation rate from 1998 to 2022 (y.o.y)',path = 'description_plot.png')
 
-    plot_decomposition(time_series= inf,var_name='Inflation_rate',path = 'decomposition_plot.png')
+    # plot_decomposition(time_series= inf,var_name='Inflation_rate',path = 'decomposition_plot.png')
 
-    plot_heatmap(time_series=inf, name='CPI(%)',path ='cpi_inflation_heatmap.png')
+    # plot_heatmap(time_series=inf, name='CPI(%)',path ='cpi_inflation_heatmap.png')
 
     data_split(time_series=inf)
 
-    plot_description(time_series=inf,title='Inflation rate (Train,Test,Vlidation)',path='training_dataset.png',if_split=True)
+    plot_description(time_series=inf,title='Inflation rate (Train,Test,Vlidation)',path='train_val_test_dataset.png',if_split=True)
                  
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -2,22 +2,60 @@ import os
 import pandas as pd
 from helper import *
 
+import warnings
+warnings.filterwarnings('ignore')
+tf.config.run_functions_eagerly(True)
+
 def main():
     cwd = os.getcwd()
     
     test = pd.read_csv(cwd+'\\results\\data\\testing_dataset.csv',index_col='date',parse_dates=['date'])
     
     models  = ['LSTM','DNN','RNN','CNN'] 
+    metrics = ['mse','rmse','mae']
+    horizons= [6,12,18,24]
     
+    results = pd.DataFrame(columns=['model', 'horizon', 'metric', 'value'])
+   
     for model in models:
+
+            model_ = models_loader(model)
+            forecasts = model_forecast(model = model_ , series = test.values)
+            actual = test[12 - 1 :].values
         
-        model_ = models_loader(model)
-        forecasts = model_forecast(model = model_ , series = test.values)
-        
-        actual = test[12 - 1 :]
-        loss = loss_comp(forecasts, actual.values.reshape(-1))
-        plot_series((test.index)[12 - 1 :],(actual, forecasts),fig_name= f'Actual vs predicted values of {model} model (testing_dataset)',legend=["Actual_values", "Predicted_values"],title=f" Actual vs predicted values of {model} (testing dataset) ",loss =loss)
-        print(f'Mean Squared Error of prediction of {model} is:',loss)
+            # Calculate the prediction intervals
+            errors = actual - forecasts
+            std_dev = np.std(errors)
+            z_value = 1.96 # For 95% confidence interval
+            prediction_interval = z_value * std_dev
+            
+            
+            # Plot the predicted values and their associated prediction intervals
+            fig =plt.figure(figsize=(8,8))
+            plt.plot(test[12 - 1 :].index,actual, label='Actual')
+            plt.plot(test[12 - 1 :].index,forecasts, label='Predicted')
+            plt.fill_between(test[12 - 1 :].index, 
+                            forecasts - prediction_interval, 
+                            forecasts + prediction_interval, 
+                            color='gray', 
+                            alpha=0.2,
+                            label='95% CI')
+            plt.title(f'Forecasted Values of {model} model with 95% Confidence Interval')
+            plt.xlabel('Months')
+            plt.ylabel('Inflation')
+            plt.xticks(rotation= 20)
+            plt.legend(loc='best')
+
+            # Save the figure
+            fig.savefig(cwd + f'\\results\\graphs\\Actual vs predicted values of {model} model (testing_dataset).png',dpi=300, bbox_inches='tight')
+            
+
+            for metric in metrics:
+                for h in horizons:
+                 loss = loss_comp(forecasts[:h], actual.reshape(-1)[:h],loss = metric)
+                # print(f'{metric}_{model} is:',loss)
+                 results = results.append({'model': model, 'horizon': h, 'metric': metric, 'value': loss}, ignore_index=True)
+    results.to_csv(cwd +f'\\results\\forecasting performence.csv') 
 
 if __name__ == "__main__":
     main()
